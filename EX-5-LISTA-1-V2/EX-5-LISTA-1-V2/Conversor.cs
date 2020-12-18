69838f4ca99c89cdd2fbfc731a08d2b61e8b230f
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-12-18 20:58:24 -0300

    Add files via upload

---
+++ EX-5-LISTA-1-V2/EX-5-LISTA-1-V2/Conversor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_5_LISTA_1_V2
+{
+    class Conversor
+    {
+        #region atributos
+        private double milha;
+        private double km;
+        #endregion
+        #region construtor
+        public Conversor() 
+        {
+            milha = 0;
+            calcularKm();
+        }
+        #endregion
+        #region set e gets
+        public void setMilha(double milha) 
+        {
+            this.milha = milha;
+        }
+        public Double getMilha()
+        {
+            return milha;
+        }
+        public Double getKm()
+        {
+            return km;
+        }
+        #endregion
+        #region metodos
+        public void calcularKm()
+        {
+            km= (milha * 1852) / 1000;
+        }
+        #endregion
+    }
+}
---
+++ EX-5-LISTA-1-V2/EX-5-LISTA-1-V2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX_5_LISTA_1_V2
+{
+    public partial class Form1 : Form
+    {
+        private Conversor conversor = new Conversor();
+        public Form1()
+        {
+            InitializeComponent();
+            txt_milha.Text = conversor.getMilha().ToString();
+            txt_km.Text = conversor.getKm().ToString();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            conversor.setMilha(double.Parse(txt_milha.Text));
+            conversor.calcularKm();
+            txt_km.Text = conversor.getKm().ToString();
+        }
+    }
+}
